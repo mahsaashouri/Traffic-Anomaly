c6336d23447ba787540509847cd00546cc87669f
Mahsa Ashouri <mahsa.ashouri@iss.nthu.edu.tw>
2023-11-05 18:04:15 -0500

    Added ARIMA

---
+++ ARIMA.R
@@ -0,0 +1,248 @@
+
+library(tidyverse)
+library(forecast)
+source('smatrix.R')
+source('CG-shrink.R')
+
+hour2021 <- read.csv('hour2021-final.csv', header = TRUE)
+hour2021 <- subset(hour2021, Freeway!= 'No5')
+
+hour2021$Station.code <- as.numeric(as.factor(hour2021$Station))
+hour2021$Freeway.code <- as.numeric(as.factor(hour2021$Freeway))
+hour2021$Area.code <- as.numeric(as.factor(hour2021$Area))
+
+
+## fixing the station codes
+hour2021$Station.code <- sprintf("%03d", as.numeric(hour2021$Station.code))
+hour2021$vehicle.type <- sprintf("%02d", as.numeric(hour2021$vehicle.type))
+
+hour2021$cat <- paste(hour2021$sensor, hour2021$direction, hour2021$vehicle.type,  hour2021$Station, hour2021$Freeway, hour2021$Area, hour2021$Region)
+### removing just zero series
+hour2021 <- hour2021 %>%
+  group_by(cat) %>%
+  filter(any(sum(traffic != 0)))
+### removing 90% zero series
+hour2021 <- hour2021 %>%
+  group_by(cat) %>%
+  filter(mean(traffic == 0) <= 0.8)
+
+
+
+### 
+## number of series before removing No5 and 80 percent zero series: 1590 - # of series after removing No5 and 90 percent zero series 1591
+hourmatrix <- hour2021$traffic %>%
+  matrix(nrow = 2880, ncol = 1590) %>%
+  as.data.frame() 
+
+
+names <- unique(paste(substr(hour2021$Region, 1, 1), hour2021$Station.code,  hour2021$Freeway.code, hour2021$direction, hour2021$vehicle.type, sep = "")) 
+colnames(hourmatrix) <- names
+
+
+name_length <- str_length(names)
+grouping_hts <- rbind(
+  #Region
+  str_sub(names, start = name_length - 7, end = name_length - 7),
+  #Station
+  str_sub(names, start = name_length - 7, end = name_length - 4),
+  #Freeway
+  str_sub(names, start = name_length - 3, end = name_length - 3),
+  #Direction
+  str_sub(names, start = name_length - 2, end = name_length - 2),
+  #Vehicle
+  str_sub(names, start = name_length - 1, end = name_length),
+  #Region x Direction
+  paste(str_sub(names, start = name_length - 7, end = name_length - 7), str_sub(names, start = name_length - 2, end = name_length - 2), sep = ""),
+  #Region x Freeway
+  paste(str_sub(names, start = name_length - 7, end = name_length - 7), str_sub(names, start = name_length - 3, end = name_length - 3), sep = ""),
+  #Region x Vehicle
+  paste(str_sub(names, start = name_length - 7, end = name_length - 7), str_sub(names, start = name_length - 1, end = name_length), sep = ""),
+  #Freeway x Direction
+  paste(str_sub(names, start = name_length - 3, end = name_length - 3), str_sub(names, start = name_length - 2, end = name_length - 2), sep = ""),
+  #Freeway x Vehicle
+  paste(str_sub(names, start = name_length - 3, end = name_length - 3), str_sub(names, start = name_length - 1, end = name_length), sep = ""),
+  #Vehicle x Direction
+  paste(str_sub(names, start = name_length - 1, end = name_length), str_sub(names, start = name_length - 2, end = name_length - 2), sep = "")
+)
+
+library(hts)
+hourgts <- gts(hourmatrix, groups = grouping_hts) 
+gmat <- GmatrixG(hourgts$groups)
+smatrix <- as.matrix(SmatrixM(gmat))
+
+allyhour2021 <- aggts(hourgts) 
+allyhour2021 <- forecast::msts(allyhour2021, seasonal.periods=c(24,24*7))
+
+n <- 2880
+t <- 240
+h <- 24
+sim <- 2000
+library(future.apply)
+plan(multisession, workers = 7)
+result.all <- list()
+sample.path <- list()
+error.train.all <- NULL
+start.time <- Sys.time()
+for(j in 1:ncol(allyhour2021)){
+  result <- NULL
+  sample.test <- NULL
+  error.train <- NULL
+  fc.test <- NULL
+  for(i in 0:((n-t-h)/h)){
+    train <- window(allyhour2021[,j], start = c(1, ((i*h)+1)), end = c(1, (t + (i*h))))
+    valid <- window(allyhour2021[,j], start = c(1,( t + (i*h) + 1)), end = c(1, (t + (i*h) +h)))
+    f.model <- auto.arima(train,  seasonal.test = "ocsb", xreg=fourier(train, K = c(3,2))) 
+    f.error.train <- as.data.frame(c(train)) - as.data.frame(c(forecast(f.model, xreg=fourier(train, K = c(3,2)))$mean))
+    f.fc.test <- forecast(f.model, xreg=fourier(train,K=c(3,2), h = h))$mean
+    sample.paths <- matrix(NA, ncol = 2000, nrow =  24)
+    for (g in 1:sim) {
+      #set.seed(g)
+      sample.paths[,g] <- simulate(f.model, future = TRUE, nsim=h, xreg=fourier(train,K=c(3,2), h = h))
+    }
+    sample.test <- rbind(sample.test, sample.paths)
+    fc.test <- rbind(fc.test, as.data.frame(c(f.fc.test)))
+    error.train <- rbind(error.train, f.error.train)
+    print(j)
+  }
+  result.all[[length(result.all)+1]] <- fc.test
+  sample.path[[length(sample.path)+1]] <- sample.test
+  error.train.all <- bind_cols(error.train.all, error.train)
+}
+end.time <- Sys.time()
+time.taken <- end.time - start.time
+
+#### saving results
+fc.ARIMA <- matrix(NA, ncol = ncol(allyhour2021), nrow = nrow(result.all[[1]]))
+for(i in 1:ncol(allyhour2021)){
+  fc.ARIMA[,i] <- result.all[[i]][,1]
+}
+
+write.csv(fc.ARIMA, 'fc.ARIMA.csv')
+
+
+## error train
+write.csv(error_train, 'error.train.ARIMA.csv')
+
+## sample path
+for(i in 1:length(sample.path)){
+  write.csv(sample.path[[i]], paste0(i,".csv"))
+}
+
+
+library(rio)
+
+mydataf <- lapply(1:ncol(sample.path[[1]]), function(y) as.data.frame(sapply(sample.path, function(x) x[, y])))
+
+
+# name the data frames
+names(mydataf) <- 1:length(mydataf)
+
+fc.ARIMA.res <- list()
+for(i in 0:(n-t-h)/h){
+  fc.ARIMA.res[[i+1]] <- error.train.all[(1+((t-h)*i)):((t-h)+((t-h)*i)),]
+}
+
+library(Matrix)
+start.time <- Sys.time()
+list.G <- list()
+for(i in 1:length(fc.ARIMA.res)){
+  res <- as.matrix(fc.ARIMA.res[[i]])
+  tar <- lowerD(res)
+  shrink <- shrink.estim(res, tar)
+  w.1 <- shrink[[1]]
+  lambda <- shrink[[2]]
+  weights <-  methods::as(w.1, "sparseMatrix")
+  list.G[[length(list.G)+1]] <- weights
+  print(i)
+}
+
+list.sample.rec <- list()
+for(i in 1:length(mydataf)){
+  fit.test <- split(mydataf[[i]], rep(1:length(fc.ARIMA.res), each=(24)))
+  result.path <- NULL
+  for(j in 1:length(list.G)){
+    result.path <- t(CG(fit.test[[j]], smatrix, weights = list.G[[j]]))
+  }
+  list.sample.rec[[length(list.sample.rec)+1]] <- result.path
+  print(i)
+}
+
+# name the data frames
+
+names(list.sample.rec) <- 1:length(list.sample.rec)
+
+#library(abind)
+#test <- abind(list.sample.rec, along = 3)
+
+#quan05 <- apply(test, 1:2, function(x) quantile(x, probs = c(0.05)))
+#quan95 <- apply(test, 1:2, function(x) quantile(x, probs = c(0.95)))
+#quan975 <- apply(test, 1:2, function(x) quantile(x, probs = c(0.975)))
+#quan25 <- apply(test, 1:2, function(x) quantile(x, probs = c(0.025)))
+
+## Computing prediction intervals - 95% and 99%
+mydataf2 <- lapply(1:ncol(list.sample.rec[[1]]), function(y) as.data.frame(sapply(list.sample.rec, function(x) x[, y])))
+
+quan975 <- matrix(NA, ncol = length(mydataf2), nrow = nrow(mydataf2[[1]]))
+
+for(i in 1:length(mydataf2)){
+  quan975[,i] <-  apply(mydataf2[[i]], 1, quantile, probs= c(0.975))
+}
+
+quan25 <- matrix(NA, ncol = length(mydataf2), nrow = nrow(mydataf2[[1]]))
+
+for(i in 1:length(mydataf2)){
+  quan25[,i] <-  apply(mydataf2[[i]], 1, quantile, probs= c(0.025))
+}
+
+quan95 <- matrix(NA, ncol = length(mydataf2), nrow = nrow(mydataf2[[1]]))
+
+for(i in 1:length(mydataf2)){
+  quan95[,i] <-  apply(mydataf2[[i]], 1, quantile, probs= c(0.95))
+}
+
+quan05 <- matrix(NA, ncol = length(mydataf2), nrow = nrow(mydataf2[[1]]))
+
+for(i in 1:length(mydataf2)){
+  quan05[,i] <-  apply(mydataf2[[i]], 1, quantile, probs= c(0.05))
+}
+
+
+write.csv(quan05, 'quan05.csv')
+write.csv(quan95, 'quan95.csv')
+write.csv(quan975, 'quan975.csv')
+write.csv(quan25, 'quan25.csv')
+
+
+## Reconciling base forecasts
+colnames(fc.ARIMA) <- colnames(allyhour2021)
+fit.list <- list()
+for(i in 0:(n-t-h)/h){
+  fit.list[[i+1]] <- fc.ARIMA[(1+(h*i)):(h+(h*i)),]
+}
+
+list <- list()
+start.time <- Sys.time()
+for(i in 1:length(fc.ARIMA.res)){
+  res <- as.matrix(fc.ARIMA.res[[i]])
+  tar <- lowerD(res)
+  shrink <- shrink.estim(res, tar)
+  w.1 <- shrink[[1]]
+  lambda <- shrink[[2]]
+  weights <-  methods::as(w.1, "sparseMatrix")
+  fc.rec <- t(CG(fit.list[[i]], smatrix, weights = weights))
+  colnames(fc.rec) <- colnames(allyhour2021) 
+  list[[length(list)+1]] <- fc.rec
+  print(i)
+}
+
+end.time <- Sys.time()
+time.taken.forecast.rec <- end.time - start.time
+
+result.fit.all.rec <- do.call(rbind.data.frame, list)
+
+write.csv(result.fit.all.rec, 'fc.ARIMA.rec.csv')
+
+### print computation times
+time.taken.base
+time.taken.sample.path.rec
+time.taken.forecast.rec
